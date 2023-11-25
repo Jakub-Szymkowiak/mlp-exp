ce0aecdcdae7cdc87285792433f5fd2e827c4ac8
Jakub Szymkowiak <jakubszymkowiak@icloud.com>
2023-11-25 13:30:33 +0100

    find packages fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='nnes',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(include=['nnes', 'nnes.*']),
     install_requires=[
         'torch'
     ],
