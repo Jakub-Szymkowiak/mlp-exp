3c26d18cff8e937ac6effe50a4589724b347d663
Jakub Szymkowiak <jakubszymkowiak@icloud.com>
2023-11-25 19:56:39 +0100

    fixes

--- nnes/pipeline/callbacks.py
+++ nnes/pipeline/callbacks.py
@@ -10,10 +10,8 @@ class EpochCallback:
         self.callbacks = callbacks
 
     def __call__(self, train_setup: 'TrainSetup', epoch: int):
-        callback_output = []
         for callback in self.callbacks:
             output = callback(self.config, train_setup, epoch)
-            callback_output.append(output)
 
     def set_config(self, config: Dict):
         self.config = config
