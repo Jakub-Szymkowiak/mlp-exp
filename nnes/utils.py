f0f7b326cc56c20b00c62cb03cd0fbc7887f9488
Jakub Szymkowiak <jakubszymkowiak@icloud.com>
2023-11-26 17:18:29 +0100

    added utils

--- nnes/utils.py
+++ nnes/utils.py
@@ -1,6 +1,5 @@
 import torch
 
-from torchvision.transforms import ToPILImage
 from torch import Tensor
 
 from typing import Tuple
@@ -8,7 +7,7 @@ from typing import Tuple
 
 def get_tensors_from_image(image: Tensor) -> Tuple[Tensor, Tensor]:
     H, W = image.shape
-    yy, xx = torch.meshgrid(torch.arange(H), torch.arange(W), indexing='ij')
+    xx, yy = torch.meshgrid(torch.arange(H), torch.arange(W), indexing='ij')
     xx_normalized, yy_normalized = xx.float() / (W-1), yy.float() / (H - 1)
 
     X = torch.stack([xx_normalized, yy_normalized], dim=-1).view(-1, 2)
@@ -18,9 +17,3 @@ def get_tensors_from_image(image: Tensor) -> Tuple[Tensor, Tensor]:
 
 def get_image_from_tensor(Y: Tensor, image_shape: Tuple) -> Tensor:
     return 255 * Y.view(*image_shape)
-
-def display_tensor_image(image: Tensor) -> None:
-    to_pil = ToPILImage()
-    pil_image = to_pil(image)
-    pil_image.show()
-
